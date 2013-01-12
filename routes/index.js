942818ce227777196ed0e8859c799d56492aec94
Thomas Peklak <thomas.peklak@gmail.com>
2013-01-12 21:58:46 +0100

    replace input fields with contenteditable elements

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var Page = require('../models/page');
 exports.loadPage = function(req,res, next) {
     Page.findOne({ path : req.path }, function(err, page) {
        if (err) { return next(err); }
-        
+
         res.locals.page = page;
         next();
     });
@@ -11,7 +11,7 @@ exports.loadPage = function(req,res, next) {
 
 exports.showPage = function(req, res) {
     if (!res.locals.page) {
-        res.locals.page = new Page({ title : "new page", tags: [], content: "" });
+        res.locals.page = new Page({ title : "new page", tags: "add tags as comma seperated list", content: "Content" });
     }
 
     return res.render("page", res.locals.page);
@@ -19,16 +19,16 @@ exports.showPage = function(req, res) {
 
 exports.savePage = function(req, res) {
     var page = res.locals.page;
-    
+
     if (!page) {
         page= new Page(req.body);
         page.path = req.path;
     } else {
         page.title = req.body.title;
         page.content = req.body.content;
-        page.tags = req.body.tags;        
+        page.tags = req.body.tags;
     }
-    
+
     page.save(function(err) {
         // TODO: Err!
         res.send(200);
