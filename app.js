09e75be0631c5f5a01d679af14dc8969a50bd282
saintelama <christoph.walcher@gmail.com>
2013-01-13 09:55:44 +0100

    Fix order of static and router to let static to handle static files first

--- app.js
+++ app.js
@@ -27,8 +27,9 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(app.router);
+
   app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
 });
 
 app.configure('development', function(){
