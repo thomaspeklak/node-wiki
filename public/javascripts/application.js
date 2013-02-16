5c0709784d057d4d9bfe51ee3f0b7f16eb7103c3
Thomas Peklak <thomas.peklak@gmail.com>
2013-02-16 15:09:00 +0100

    hightlight title and tags if they are not yet changed to be easily replaced

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -144,6 +144,76 @@ function readCookie(name) {
 
 }(CKEDITOR));
 
+(function ($) {
+    //if not changed highlight text for easy replacement
+    function getTextNodesIn(node) {
+        var textNodes = [];
+        if (node.nodeType == 3) {
+            textNodes.push(node);
+        } else {
+            var children = node.childNodes;
+            for (var i = 0, len = children.length; i < len; ++i) {
+                textNodes.push.apply(textNodes, getTextNodesIn(children[i]));
+            }
+        }
+        return textNodes;
+    }
+
+    function setSelectionRange(el, start, end) {
+        if (document.createRange && window.getSelection) {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            var textNodes = getTextNodesIn(el);
+            var foundStart = false;
+            var charCount = 0, endCharCount;
+
+            for (var i = 0, textNode; textNode = textNodes[i++]; ) {
+                endCharCount = charCount + textNode.length;
+                if (!foundStart && start >= charCount
+                    && (start < endCharCount ||
+                        (start == endCharCount && i < textNodes.length))) {
+                        range.setStart(textNode, start - charCount);
+                        foundStart = true;
+                    }
+                    if (foundStart && end <= endCharCount) {
+                        range.setEnd(textNode, end - charCount);
+                        break;
+                    }
+                    charCount = endCharCount;
+            }
+
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (document.selection && document.body.createTextRange) {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.collapse(true);
+            textRange.moveEnd("character", end);
+            textRange.moveStart("character", start);
+            textRange.select();
+        }
+    }
+
+    $(".tags .edit").focus(function () {
+        if (this.innerText == "add tags as comma separated list") {
+            var el = this;
+            setTimeout(function () {
+                setSelectionRange(el, 0, el.innerText.length);
+            }, 30);
+        }
+    });
+
+    $("h1.edit").focus(function () {
+        if (this.innerText == "new page") {
+            var el = this;
+            setTimeout(function () {
+                setSelectionRange(el, 0, el.innerText.length);
+            }, 30);
+        }
+    });
+}(jQuery));
+
 (function(app) {
     app.ProgressBar = function (target, upload) {
         var self = this;
