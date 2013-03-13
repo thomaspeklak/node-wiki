74c40e7f9be91d0966516d8bf5e2383ddb0be972
Thomas Peklak <thomas.peklak@gmail.com>
2013-03-13 17:50:45 +0100

    use DB environment variable if present

--- app.js
+++ app.js
@@ -1,52 +1,53 @@
 "use strict";
 
-var express = require('express')
-, http = require('http')
-, mongoose = require('mongoose');
+var express = require("express"),
+    http = require("http"),
+    mongoose = require("mongoose");
 
-// TODO: Refactor db connection string
-mongoose.connect('mongodb://localhost/nodewiki', function(err) {
+mongoose.connect(process.env.DB || "mongodb://localhost/nodewiki", function (err) {
     if (err) {
-        console.log('Could not connect to database "' +
-                    'mongodb://localhost/nodewiki' +
-                    '". Ensure that a mongo db instance is up and running.');
+        console.log("Could not connect to database \"" +
+            "mongodb://localhost/nodewiki" +
+            "\". Ensure that a mongo db instance is up and running.");
         console.log(err);
         process.exit(1);
     }
 });
-require('express-mongoose');
+require("express-mongoose");
 
 var app = express();
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
+    app.set("port", process.env.PORT || 3000);
+    app.set("views", __dirname + "/views");
+    app.set("view engine", "jade");
     app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(express.bodyParser({uploadDir: __dirname + "/uploads"}));
+    app.use(express.logger("dev"));
+    app.use(express.bodyParser({
+        uploadDir: __dirname + "/uploads"
+    }));
     app.use(express.cookieParser());
     app.use(express.methodOverride());
 
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + "/public"));
     app.use(app.router);
 });
 
-app.configure('development', function () {
+app.configure("development", function () {
     app.use(express.errorHandler());
 });
 
 app.all("*", require("./middleware/load-navigation"));
 app.all("*", require("./middleware/build-breadcrumbs"));
-app.all('*', require("./middleware/load-page"));
+app.all("*", require("./middleware/load-page"));
 
 require("./routes")(app);
 
-http.createServer(app).listen(app.get('port'), function () {
-    console.log("Express server listening on port " + app.get('port'));
+http.createServer(app).listen(app.get("port"), function () {
+    console.log("Express server listening on port " + app.get("port"));
 });
 
-if(process.env.NODE_ENV == "development" || !process.env.NODE_ENV){
+if (process.env.NODE_ENV == "development" ||  !process.env.NODE_ENV) {
     require("./lib/live-reload")();
 }
