1c59249a1d61b640fb93176af062bd8355558ede
Thomas Peklak <thomas.peklak@gmail.com>
2013-03-13 17:54:38 +0100

    add xml to allowed content types

--- config/supported-mediatypes.js
+++ config/supported-mediatypes.js
@@ -2,22 +2,23 @@
 
 module.exports = {
     media: [
-        "text/plain",
-        "application/zip",
         "application/pdf",
+        "application/xml",
+        "application/zip",
+        "audio/mp3",
+        "audio/mp4",
+        "audio/ogg",
         "image/gif",
         "image/jpeg",
         "image/pjpeg",
         "image/png",
         "image/svg+xml",
         "text/csv",
-        "audio/mp3",
-        "audio/mp4",
-        "audio/ogg",
-        "video/webm",
+        "text/plain",
         "video/mp4",
-        "video/ogv"
-    ], 
+        "video/ogv",
+        "video/webm"
+    ],
     images: [
         "image/gif",
         "image/jpeg",
