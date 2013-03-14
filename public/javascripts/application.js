7a36b87c1cf775520e81868ec4a0fb43a6e266ad
Thomas Peklak <thomas.peklak@gmail.com>
2013-03-14 22:43:14 +0100

    fix some whitespace issues

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -585,12 +585,12 @@ function readCookie(name) {
 (function ($){
     // Load Images before show them - pages/covers
     // Fix cached images issues
-    $(".img-polaroid").one('load', function() {
-        $(".preview").each(function(index){
-            $(this).delay(100*index).fadeIn(200);
-        })
-    }).each(function() {
-        if(this.complete) $(this).load();
+    $(".img-polaroid").one("load", function () {
+        $(".preview").each(function (index) {
+            $(this).delay(100 * index).fadeIn(200);
+        });
+    }).each(function () {
+        if (this.complete) $(this).load();
     });
 }(jQuery));
 
