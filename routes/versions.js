ed829c7d1c45f0943fd7b3ae2f9ee0970a809371
Thomas Peklak <thomas.peklak@gmail.com>
2013-05-07 21:09:55 +0200

    show versions

--- routes/versions.js
+++ routes/versions.js
@@ -2,6 +2,18 @@
 
 var Page = require("../models/page");
 
+var next = function (versions, id) {
+    for (var i = 0; i < versions.length; i++) {
+        if (versions[i]._id == id && versions[i + 1]) return versions[i + 1];
+    }
+};
+
+var previous = function (versions, id) {
+    for (var i = 0; i < versions.length; i++) {
+        if (versions[i]._id == id && versions[i - 1]) return versions[i - 1];
+    }
+};
+
 module.exports = function (app) {
     app.get("/versions", function (req, res) {
         Page.VersionedModel.latest(100, function (err, pages) {
@@ -30,4 +42,30 @@ module.exports = function (app) {
         });
     });
 
+    app.get("/versions/:pageId/:version", function (req, res) {
+        Page.VersionedModel.findOne({
+            refId: req.params.pageId
+        }).exec(function (err, history) {
+            if (err) {
+                console.error(err);
+                res.send(500);
+            }
+
+            var version = history.versions.id(req.params.version);
+
+            if (!version) {
+                res.send(404);
+            }
+
+            res.render("version", {
+                version: version,
+                page: Page.findOne({
+                    _id: req.params.pageId
+                }),
+                title: "Version for " + version.title,
+                next: next(history.versions, req.params.version),
+                previous: previous(history.versions, req.params.version),
+            });
+        });
+    });
 };
