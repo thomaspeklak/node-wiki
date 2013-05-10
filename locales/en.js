4a1ff5e3b272270036ffcb382dd5894bf40b01fd
Thomas Peklak <thomas.peklak@gmail.com>
2013-05-11 00:21:54 +0200

    prepare views for i18n

--- locales/en.js
+++ locales/en.js
@@ -2,5 +2,6 @@
 	"No static navigation found. Create page 'navigation' first.": "No static navigation found. Create page 'navigation' first.",
 	"click here and enter page title": "click here and enter page title",
 	"add tags as comma separated list": "add tags as comma separated list",
-	"click here and enter new content...": "click here and enter new content..."
+	"click here and enter new content...": "click here and enter new content...",
+	"All Pages": "All Pages"
 }
