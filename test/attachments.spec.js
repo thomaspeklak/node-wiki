cefe031d8e18850c29381fbb6b7962078fb21b00
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-02 22:08:43 +0200

    complete tests for attachments and images

--- test/attachments.spec.js
+++ test/attachments.spec.js
@@ -52,4 +52,100 @@ describe("Uploads", function () {
             });
         });
     });
+
+    it("should reject unsupported attachment media types", function (done) {
+        request(app)
+            .post("/attachments")
+            .set("Referer", "/foobar")
+            .attach("attachments", __filename)
+            .expect(415)
+            .end(done);
+    });
+
+    it("should upload images", function (done) {
+        request(app)
+            .post("/images")
+            .set("Referer", "/foobar")
+            .attach("images", path.join(__dirname, "attachments/noimg.png"))
+            .expect(200)
+            .end(function (err, res) {
+            expect(res.text).to.include("noimg.png");
+
+            request(app)
+                .get("/foobar")
+                .end(function (err, res) {
+
+                expect(res.text).to.include("noimg.png");
+
+                done(err);
+            });
+        });
+    });
+
+    it("should reject unsupported image media types", function (done) {
+        request(app)
+            .post("/images")
+            .set("Referer", "/foobar")
+            .attach("images", __filename)
+            .expect(415)
+            .end(done);
+    });
+
+    it("should delete attachments", function (done) {
+        request(app)
+            .post("/attachments")
+            .set("Referer", "/foobar")
+            .attach("attachments", path.join(__dirname, "attachments/noimg.png"))
+            .expect(200)
+            .end(function (err) {
+            expect(err).to.not.exist;
+
+            request(app)
+                .del("/attachments")
+                .set("Referer", "/foobar")
+                .send({
+                "file": "noimg.png"
+            })
+                .expect(200)
+                .end(function (err, res) {
+                expect(res.text).to.include("lastModified");
+                expect(err).to.not.exist;
+                request(app)
+                    .get("/foobar")
+                    .end(function (err, res) {
+                    expect(res.text).to.not.include("noimg.png");
+                    done(err);
+                });
+            });
+        });
+    });
+
+    it("should delete images", function (done) {
+        request(app)
+            .post("/images")
+            .set("Referer", "/foobar")
+            .attach("images", path.join(__dirname, "attachments/noimg.png"))
+            .expect(200)
+            .end(function (err) {
+            expect(err).to.not.exist;
+
+            request(app)
+                .del("/images")
+                .set("Referer", "/foobar")
+                .send({
+                "file": "noimg.png"
+            })
+                .expect(200)
+                .end(function (err, res) {
+                expect(res.text).to.include("lastModified");
+                expect(err).to.not.exist;
+                request(app)
+                    .get("/foobar")
+                    .end(function (err, res) {
+                    expect(res.text).to.not.include("noimg.png");
+                    done(err);
+                });
+            });
+        });
+    });
 });
