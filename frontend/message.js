4627393424b9be510c2042131c038302920cbc17
Thomas Peklak <thomas.peklak@gmail.com>
2013-06-07 07:33:27 +0200

    update hbsfy use handlebars for message tempate

--- frontend/message.js
+++ frontend/message.js
@@ -1,6 +1,7 @@
 "use strict";
-
 //provide an interface to display a message to the user
+
+var messageTemplate = require("./templates/message.hbs");
 
 function remove() {
     $(this).remove();
@@ -8,14 +9,13 @@ function remove() {
 
 module.exports = function (type, message, delay) {
     delay = delay ||  5e3;
-    var html = '<div class="alert alert-' + type + '"> \
-            <button type="button" class="close" data-dismiss="alert">&times;</button> \
-            ' + message + '\
-            </div>';
+    var html = messageTemplate({
+            type: type,
+            message: message
+        });
 
     $(html)
         .appendTo('#messages')
         .delay(delay)
         .fadeOut(remove);
 };
-
